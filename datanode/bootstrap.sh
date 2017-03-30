b2e41dd834d88f7f0b0dafe919ceba753a219422
witlox <pim.witlox@gmail.com>
2017-03-30 16:01:33 +0200

    changed to debs

--- datanode/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-# set hadoop environment
-$HADOOP_HOME/etc/hadoop/hadoop-env.sh
-
-# installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_HOME/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
-
-service sshd start
-
-$HADOOP_HOME/sbin/start-dfs.sh
